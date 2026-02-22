b2ca6f58e9119d035363d10955b9524707782a6e
alrod43 <alexrodri43@outlook.com>
2026-02-22 09:54:22 -0500

    Move login page to /login

---
+++ stemApp/app/login/page.js
@@ -0,0 +1,14 @@
+'use client';
+import LogIn from '../components/LogIn';
+
+export default function LogInPage() {
+  return (
+    <main className="login-container">
+      <h2>Log In</h2>
+      <LogIn />
+      <h5>
+        Not a User? Please <a href="/register"><span>Register</span></a> here first
+      </h5>
+    </main>
+  );
+}
