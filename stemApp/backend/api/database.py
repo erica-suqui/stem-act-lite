b82c78f3891dac4abc892535b52c06cd0efe8da6
alrod43 <alexrodri43@outlook.com>
2026-04-25 14:06:45 -0400

    edit: update pass var within database.py to correct title

--- stemApp/backend/api/database.py
+++ stemApp/backend/api/database.py
@@ -1,5 +1,6 @@
 from google.cloud.sql.connector import Connector
 import sqlalchemy
+import os
 
 def create_engine():
     connector = Connector()
@@ -9,7 +10,7 @@ def create_engine():
             "storied-precept-491812-u8:us-east1:stemact-db",
             "pg8000",
             user="stemact-user",
-            password=os.getenv("PASSWORD"),
+            password=os.getenv("DB_PASSWORD"),
             db="stemact-events",
         )
         return conn
@@ -19,3 +20,8 @@ def create_engine():
         creator=getconn,
     )
     return engine
+
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
